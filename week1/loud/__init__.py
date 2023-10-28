743daf6a4c7d6e73e773d367fc348aa890a1392a
Nielsen Fotis <113636339+LeMoose6@users.noreply.github.com>
2023-10-28 17:34:51 -0400

    Create __init__.py

---
+++ week1/loud/__init__.py
@@ -0,0 +1,24 @@
+import check50
+
+@check50.check()
+def hello():
+  """input of "hello" outputs "HELLO""""
+  check50.run("python loud.py").stdin("hello").stdout("HELLO").exit(0)
+
+
+@check50.check()
+def punctuation():
+  """input with exclamation point does not affect outcome"""
+  check50.run("python loud.py").stdin("hello!").stdout("HELLO!").exit(0)
+
+
+@check50.check()
+def already_cpas():
+  """already capitalized stay capitalized"""
+  check50.run("python loud.py").stdin("Hello").stdout("HELLO").exit(0)
+
+
+@check50.check()
+def numeric():
+  """program still works with numbers"""
+  check50.run("python loud.py").stdin("hello123").stdout("HELLO123").exit(0)
